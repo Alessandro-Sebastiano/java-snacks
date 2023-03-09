2893fd540aa4a2668cf6ab0fd75c3c438974995d
Alessandro-Sebastiano <alesebastiano15@gmail.com>
2023-03-09 14:17:17 +0100

    snack-1

--- src/org/lessons/java/Snack1.java
+++ src/org/lessons/java/Snack1.java
@@ -1,7 +1,19 @@
 package org.lessons.java;
 
+import java.util.Scanner;
+
 public class Snack1 {
 
+    public static void main(String[] arg){
 
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Inserisci un numero.");
+        System.out.print("Number: ");
+        int inputNumber = Integer.parseInt(input.nextLine());
+
+        System.out.println((inputNumber % 2) == 0 ? inputNumber : ++inputNumber);
+
+    }
 
 }
