b717e6cd35521a7ea34d86f2bff01f92c4ecf3aa
Alessandro-Sebastiano <alesebastiano15@gmail.com>
2023-03-09 14:58:34 +0100

    scan-close

--- src/org/lessons/java/Snack1.java
+++ src/org/lessons/java/Snack1.java
@@ -11,6 +11,7 @@ public class Snack1 {
         System.out.println("Inserisci un numero.");
         System.out.print("Number: ");
         int inputNumber = Integer.parseInt(input.nextLine());
+        input.close();
 
         System.out.println((inputNumber % 2) == 0 ? inputNumber : ++inputNumber);
 
